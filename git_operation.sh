94e035acdfb65c09d19cf95637e2e40b3f77c62c
kntWT <kento.wata0628@gmail.com>
2026-03-03 16:16:08 +0900

    feat: 第二引数以降をそのままgitコマンドに渡すように

--- git_operation.sh
+++ git_operation.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 usage() {
   cat <<EOF
 Usage:
-  $0 <operation> [branch or tag]
+  $0 <operation> [args...]
 
 Options:
   -h, --help    Show this help message
 
 Arguments:
-  operation     必須引数
-  branch or tag     任意引数（省略可）
+  operation     必須引数 (pull, push, clone のいずれか)
+  args...       任意引数 (gitコマンドにそのまま渡されます)
 EOF
 }
 
@@ -34,22 +34,20 @@ GET_REMOTE_SCRIPT="${SCRIPT_DIR}/get_remote.sh"
 
 # required argument
 operation="${1}"
-# optional argument
-branch="${2:-}"
 
 remote="$($GET_REMOTE_SCRIPT)"
 
 case "${operation}" in
   "pull")
-    git pull $remote $branch
+    git pull "$remote" "${@:2}"
     ;;
   "push")
-    git push $remote $branch
+    git push "$remote" "${@:2}"
     ;;
   "clone")
-    git clone $remote $branch
+    git clone "$remote" "${@:2}"
     ;;
-  *)  
+  *)
     echo "Invalid operation: $operation"
     exit 1
     ;;
